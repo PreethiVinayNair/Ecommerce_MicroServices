445fe6fda871c406f4151e863ec15a9849c11118
Preethi Nair <preet.nair2004@gmail.com>
2020-07-02 22:31:58 +1200

    Commit

---
+++ ECommerce.Api.Orders/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Api.Orders.Models
+{
+  public class OrderItem
+  {
+    [Key]
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+
+  }
+}
